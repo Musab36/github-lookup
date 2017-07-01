a01df1b24b3d82136a3898755f6d8a85ecaa7bd9
Musab36 <musabhd@gmail.com>
2017-07-01 14:58:08 +0300

    github user lookup

--- build/js/app.js
+++ build/js/app.js
@@ -6,12 +6,12 @@ var key = require('./../.env').apiKey; // We are exporting the apiKey from .env 
 var githubRepos = function(username) { // Repos function
   // We are loading data from github when a user enters a username
     return $.get('https://api.github.com/users/' + username + '/repos?access_token=' + key).then(function(response) {
-$("#myRepos").html(" "); //
+$("#myRepos").html(" "); // We are getting the content in the id myRepos and setting the content to empty
   console.log(response);
-    if (response.length > 0) {
+    if (response.length > 0) { // A function calculating the length of the response and if  length is greater than 0 Github Respositories is appended
      var description = " ";
      $("#myRepos").append("<h1 id='userRepos'><span class='user'></span>Github Repositories</h1>");
-            $.each(response, function(index) {
+            $.each(response, function(index) { // We are looping through each response and  moments.js is used to display when a repo was created and updated
               $('.image').html('<img src="'+response.avatar_url+'">');
                 var created = moment(this.created_at).format('MMMM Do YYYY, h a');
                   var updated = moment(this.updated_at).format('MMMM Do YYYY, h a');
@@ -23,6 +23,7 @@ $("#myRepos").html(" "); //
                 })
           }
                else {
+         // We are getting all the contents that are within the user id
           $("#user").html("<h1 class='center'>USER NOT FOUND</h1>");
               $.each(response, function(i)
                     {
@@ -32,7 +33,7 @@ $("#myRepos").html(" "); //
               )}
                   }
                 )};
-
+// We are exporting Module called githubRepos
  exports.reposModule = githubRepos;
 
 },{"./../.env":1}],3:[function(require,module,exports){
