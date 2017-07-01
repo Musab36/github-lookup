181d335d24d58952bd4cd14bfa8e8f80badd96a7
Musab36 <musabhd@gmail.com>
2017-07-01 15:50:50 +0300

    github user-lookup

--- build/js/app.js
+++ build/js/app.js
@@ -40,6 +40,19 @@ $("#myRepos").html(" "); // We are getting the content in the id myRepos and set
 var githubRepos = require('./../js/user-backend.js').reposModule;
 
 $(document).ready(function() {
+  $("#mybtn").click(function() {
+    $("#mybtn").fadeOut();
+    $(".well").fadeOut();
+    $("#back").fadeIn();
+  });
+  $("#back").click(function() {
+    $("#back").fadeOut();
+    $("#myResults").fadeOut();
+    $(".well").fadeIn();
+    $("#mybtn").fadeIn();
+    $(".marqs1").show();
+    $(".marqs2").show();
+  });
   $("#myResults").hide();
   $("#userForm").submit(function(event) {
     event.preventDefault();
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,6 +1,19 @@
 var githubRepos = require('./../js/user-backend.js').reposModule;
 
 $(document).ready(function() {
+  $("#mybtn").click(function() {
+    $("#mybtn").fadeOut();
+    $(".well").fadeOut();
+    $("#back").fadeIn();
+  });
+  $("#back").click(function() {
+    $("#back").fadeOut();
+    $("#myResults").fadeOut();
+    $(".well").fadeIn();
+    $("#mybtn").fadeIn();
+    $(".marqs1").show();
+    $(".marqs2").show();
+  });
   $("#myResults").hide();
   $("#userForm").submit(function(event) {
     event.preventDefault();
